fc03c972d3323d4c4fc3dd66e2b966f229be599a
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2020-11-12 07:21:20 +0100

    Simplified..

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from os import getenv
 from os.path import abspath
 from subprocess import check_call, call, check_output
 
-remote = "origin"
+remote = "remote"
 
 
-def probe_driver_repo_for_name(driverRepoPath):
-    url = check_output(
-        ["git", "remote", "get-url", remote],
-        cwd=driverRepoPath, universal_newlines=True)
-    url = url.splitlines()[0]
-    match = re.search('neo4j-(?P<driverName>.*)-driver', url)
-    if not match:
-        return "unknown"
-    try:
-        return match.group('driverName')
-    except IndexError:
-        return "unknown"
+#def probe_driver_repo_for_name(driverRepoPath):
+#    url = check_output(
+#        ["git", "remote", "get-url", remote],
+#        cwd=driverRepoPath, universal_newlines=True)
+#    url = url.splitlines()[0]
+#    match = re.search('neo4j-(?P<driverName>.*)-driver', url)
+#    if not match:
+#        return "unknown"
+#    try:
+#        return match.group('driverName')
+#    except IndexError:
+#        return "unknown"
 
 
 def get_testkit_branch(driverName, driverTargetBranch):
@@ -30,7 +30,7 @@ def get_testkit_branch(driverName, driverTargetBranch):
 def main(testkitRepoPath, driverRepoPath, driverTargetBranch):
     testkitRepoPath = abspath(testkitRepoPath)
     driverRepoPath = abspath(driverRepoPath)
-    driverName = probe_driver_repo_for_name(driverRepoPath)
+    driverName = getenv('TEST_DRIVER_NAME') #probe_driver_repo_for_name(driverRepoPath)
     print("Driver is {} and target branch is {}".format(
         driverName, driverTargetBranch))
 
@@ -38,29 +38,32 @@ def main(testkitRepoPath, driverRepoPath, driverTargetBranch):
     print("Testkit branch is {}".format(testkitBranch))
 
     check_call(["git", "fetch", remote], cwd=testkitRepoPath)
+    check_call(["git", "checkout", "{}/{}".format(remote, testkitBranch)], cwd=testkitRepoPath)
+
+
     # Position on master to be able to remove temp branch
-    call(["git", "checkout", "master"], cwd=testkitRepoPath)
+    #call(["git", "checkout", "master"], cwd=testkitRepoPath)
     # Checkout the branch named as a temporary to avoid having to pull but
     # delete it first to make sure it is up to date with the remote.k
-    localBranch = "tempForPr"
-    call(["git", "branch", "-D", localBranch], cwd=testkitRepoPath)
-    remoteBranch = "{}/{}".format(remote, testkitBranch)
-    check_call(
-        ["git", "checkout", "-b", localBranch, remoteBranch],
-        cwd=testkitRepoPath)
+    #localBranch = "tempForPr"
+    #call(["git", "branch", "-D", localBranch], cwd=testkitRepoPath)
+    #remoteBranch = "{}/{}".format(remote, testkitBranch)
+    #check_call(
+    #    ["git", "checkout", "-b", localBranch, remoteBranch],
+    #    cwd=testkitRepoPath)
 
     # Run testkit
-    testkitEnv = {
-        "TEST_DRIVER_NAME":  driverName,
-        "TEST_DRIVER_REPO":  driverRepoPath,
-        "TEST_IN_TEAMCITY":  "1",
-        "TEST_BRANCH":       testkitBranch,
-        "TEAMCITY_USER":     getenv("TEAMCITY_USER"),
-        "TEAMCITY_PASSWORD": getenv("TEAMCITY_PASSWORD"),
-    }
-    check_call(
-        ["python3", "main.py"],
-        env=testkitEnv, cwd=testkitRepoPath, universal_newlines=True)
+    #testkitEnv = {
+    #    "TEST_DRIVER_NAME":  driverName,
+    #    "TEST_DRIVER_REPO":  driverRepoPath,
+    #    "TEST_IN_TEAMCITY":  "1",
+    #    "TEST_BRANCH":       testkitBranch,
+    #    "TEAMCITY_USER":     getenv("TEAMCITY_USER"),
+    #    "TEAMCITY_PASSWORD": getenv("TEAMCITY_PASSWORD"),
+    #}
+    #check_call(
+    #    ["python3", "main.py"],
+    #    env=testkitEnv, cwd=testkitRepoPath, universal_newlines=True)
 
 
 if __name__ == "__main__":
